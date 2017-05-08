d8537f1018a7ad11e02f00937c488bbb046bbc47
Dominic Scheirlinck <dominic@asknice.ly>
2017-05-08 12:11:52 +1200

    Better explain situation about static host
    We're doing this to work around client-side limitations in the
    generator, mostly. It's also so I can keep the OAuth flow external to
    this library mostly for selfish reasons. Patches are welcome.

--- src/HostReplacementMessageFactory.php
+++ src/HostReplacementMessageFactory.php
@@ -8,16 +8,24 @@ use Http\Message\MessageFactory;
  * Replaces a static host header when creating requests
  *
  * Unfortunately, OpenAPI before version 3 (unreleased at time of writing) does not support variable server names. The
- * `host` property is either given as a static string (no templating allowed) or is interpreted as the hostname the
+ * `host` property is either given as a static string in the JSON (no templating allowed) or is interpreted as the hostname the
  * documentation is served from.
  *
- * The JobAdder swagger contains a host of 'localapi.jobadder.com', by default, but the actual hostname you should
- * use is given by the OAuth flow. In order to allow the HTTP client that's injected to control its own Host header, we
- * ignore the static Host header passed from the generated OpenAPI code.
+ * But also, more broadly, while OpenAPI does support Security Definitions
+ * (http://swagger.io/specification/#securityDefinitionsObject) and the JobAdder OpenAPI schema correctly points to
+ * their OAuth flow, our upstream client generation library (Jane) doesn't seem to do anything with that part
+ * of the spec.
+ *
+ * So, the OAuth flow is out of scope for this client, and we rely on the user configuring the injected HTTP
+ * client with the correct hostname (as returned in the access token response leg of the OAuth flow) and
+ * Authorization header. In order to allow the HTTP client that's injected to control its own Host header, we ignore the
+ * static Host header passed from the generated OpenAPI code.
+ *
+ * Hope that explains this hackery! Improvements welcome. But definitely a client-side limitation here. :)
  */
 class HostReplacementMessageFactory implements MessageFactory
 {
-    private const STATIC_HOST = 'localapi.jobadder.com';
+    private const STATIC_HOST = 'api.jobadder.com';
 
     /**
      * @var MessageFactory
